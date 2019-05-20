96e11e8df4df6b10d4a9cfe7137322635ce9bc5d
zhangguowei <1469039806@qq.com>
2019-05-20 14:38:56 +0800

    完成退出登录

--- basic/authorization-center/src/main/java/org/auth/server/controller/LoginController.java
+++ basic/authorization-center/src/main/java/org/auth/server/controller/LoginController.java
@@ -1,7 +1,9 @@
 package org.auth.server.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.auth.server.entity.GdUser;
 import org.auth.server.mapper.GdUserMapper;
+import org.fresh.gd.commons.consts.pojo.ResponseData;
 import org.fresh.gd.commons.consts.pojo.dto.user.RoleAndUserDTO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +12,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @DATA 2019-04-12 15:20
  * @Author 张国伟  WeChat:17630376104
  * @Description
  */
+@Slf4j
 @Controller
 public class LoginController {
     @Autowired
     GdUserMapper gdUserMapper;
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "base-login";
     }
 
+
+    @RequestMapping("/exitUser")
+    public void exitUser(HttpServletResponse response, HttpServletRequest request) {
+        request.getSession().invalidate();
+        response.addCookie(new Cookie("JSESSIONID", ""));
+        response.addCookie(new Cookie("OAUTH2SESSION", ""));
+        response.addCookie(new Cookie("XSRF-TOKEN", ""));
+
+    }
+    @RequestMapping("/reUrl")
+    public String reUrl()
+    {
+        return "redirect:http://localhost:8777";
+    }
 
 }
